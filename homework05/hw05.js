92eb31908cdf4741ac87fad048f29c1a7902d29d
Jiho <133630111+jhkki034@users.noreply.github.com>
2025-04-10 16:38:07 +0900

    Update hw05.js

--- homework05/hw05.js
+++ homework05/hw05.js
@@ -1,5 +1,5 @@
-import { resizeAspectRatio, Axes } from '/util/util.js';
-import { Shader, readShaderFile } from '/util/shader.js';
+import { resizeAspectRatio, Axes } from '../util.js';
+import { Shader, readShaderFile } from '../shader.js';
 import { squarePyramid } from '/squarePyramid.js';
 
 const canvas = document.getElementById('glCanvas');
